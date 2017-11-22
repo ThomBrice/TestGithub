0d97683f2e127007de18f7cdf6581c298fedb141
ThomB <thomas.brice.59@gmail.com>
2017-11-22 17:31:44 +0100

    UI improvement

--- app/src/main/java/brice/testgithub/ui/LoginActivity.java
+++ app/src/main/java/brice/testgithub/ui/LoginActivity.java
@@ -8,10 +8,8 @@ package brice.testgithub.ui;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Toast;
 
 import brice.testgithub.Model.AccessToken;
 import brice.testgithub.Model.TokenStore;
